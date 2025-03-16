be189ae7fbe2b98b130ffd7c092a1996919f7838
Timmy1404 <timothypearman1@gmail.com>
2025-03-16 14:25:54 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1124_2425_Assessment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
